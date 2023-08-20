a20f8409710c78946e2ec6fc87821b4e5e3948d5
TariqLisse <59421188+TariqLisse@users.noreply.github.com>
2023-08-20 12:52:07 -0400

    New Changes

---
+++ Screens/Login.js
@@ -0,0 +1,96 @@
+import React, {useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+// Formik
+import {Formik} from 'formik';
+
+// Styled Components
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle,
+    SubTitle,
+    StyledFormArea,
+    LeftIcon,
+    StyledInputLabel,
+    StyledTextInput,
+    RightIcon,
+    Colors
+} from './../Components/Styles';
+
+import {View} from 'react-native';
+
+// Icons
+import {Octicons, Ionicons} from '@expo/vector-icons';
+
+// Colors
+const {brand, darkLight} = Colors;
+
+const Login = () => {
+    const [hidePassword, setHidePassword] = useState(true);
+
+    return(
+        <StyledContainer>
+            <StatusBar style = "dark" />
+            <InnerContainer>
+                <PageLogo resizeMode = "cover" source = {require('./../assets/Images/Tash.png')} />
+                <PageTitle>Elevating Humanity  Redefining Connections</PageTitle>
+                <SubTitle>Enter the Matrix</SubTitle>
+
+                <Formik
+                    initialValues = {{email: '', password: ''}}
+                    onSubmit = {(values) => {
+                        console.log(values);
+                    }}
+                >{({handleChange, handleBlur, handleSubmit, values}) => (<StyledFormArea>
+                    <MyTextInput
+                        label = "Email"
+                        icon = "mail"
+                        placeholder = "Email"
+                        placeholderTextColor = {darkLight}
+                        onChangeText = {handleChange('email')}
+                        onBlur = {handleBlur('email')}
+                        value = {values.email}
+                        keyboardType = "email-address"
+                    />
+
+                    <MyTextInput
+                        label = "Password"
+                        icon = "lock"
+                        placeholder = "* * * * * * * *"
+                        placeholderTextColor = {darkLight}
+                        onChangeText = {handleChange('password')}
+                        onBlur = {handleBlur('password')}
+                        value = {values.password}
+                        secureTextEntry = {hidePassword}
+                        isPassword = {true}
+                        hidePassword = {hidePassword}
+                        setHidePassword = {setHidePassword}
+                    />
+                </StyledFormArea>)}
+
+                </Formik>
+            </InnerContainer>
+        </StyledContainer>
+    );
+};
+
+const MyTextInput = ({label, icon, isPassword, hidePassword, setHidePassword, ...props}) => {
+    return(
+        <View>
+            <LeftIcon>
+                <Octicons name = {icon} size = {30} color = {brand} />
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+            <StyledTextInput {...props} />
+            {isPassword && (
+                <RightIcon>
+                    <Ionicons name = {hidePassword ? 'eye-off' : 'eye'} size = {30} color = {darkLight} />
+                </RightIcon>
+            )}
+        </View>
+    );
+}
+
+export default Login;
