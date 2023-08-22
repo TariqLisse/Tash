baf902e499d4393d926088b499028f4ac18439b1
TariqLisse <59421188+TariqLisse@users.noreply.github.com>
2023-08-22 18:51:43 -0400

    New Changes

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import React from 'react';
 // Screens
 import Login from './Screens/Login';
 import Signup from './Screens/Signup';
+import Welcome from './Screens/Welcome';
 
 export default function App() {
   return (
-    <Signup />
+    <Welcome />
   );
 }
 
---
+++ Screens/Welcome.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+// Styled Components
+import {
+    InnerContainer,
+    PageTitle,
+    SubTitle,
+    StyledFormArea,
+    StyledButton,
+    ButtonText,
+    Line,
+    WelcomeContainer,
+    WelcomeImage,
+    Avatar
+} from './../Components/Styles';
+
+const Welcome = () => {
+
+    return(
+        <>
+            <StatusBar style = "dark" />
+            <InnerContainer> 
+                <WelcomeImage resizeMode = "cover" source = {require('./../assets/Images/T.png')} />
+                <WelcomeContainer>
+                    <PageTitle welcome = {true}>Welcome!</PageTitle>
+                    <SubTitle welcome = {true}>Tariq Lisse</SubTitle>
+                    <SubTitle welcome = {true}>tariqlisse@gmail.com</SubTitle>
+                    <StyledFormArea>
+                    <Avatar resizeMode = "cover" source = {require('./../assets/Images/icons8-user-default-96.png')} />
+                    <Line />
+                   
+                    <StyledButton onPress = {() => {}}>
+                        <ButtonText>
+                            Logout
+                        </ButtonText>
+                    </StyledButton>
+                    
+                </StyledFormArea>
+
+                </WelcomeContainer>
+            </InnerContainer>
+        </>
+    );
+};
+
+export default Welcome;
